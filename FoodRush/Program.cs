cf3d1794e0624e6725ad3515710d9d31fe43e1fd
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 13:13:07 +0200

    feat: add Swagger to solution

--- FoodRush/Program.cs
+++ FoodRush/Program.cs
@@ -6,12 +6,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new Microsoft.OpenApi.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Best Food Ordering App"
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Best Food Ordering App v1");
+        options.RoutePrefix = "";
+    });
 }
 
 app.UseHttpsRedirection();
