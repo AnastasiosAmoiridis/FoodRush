acb8c0bb707d54fbbc24845e68506522a348f706
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 15:33:04 +0200

    feat(models): add CustomerAddress entity

---
+++ Models/Entities/CustomerAddress.cs
@@ -0,0 +1,25 @@
+﻿namespace Models.Entities
+{
+    public class CustomerAddress
+    {
+        public Guid Id { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Street { get; set; }
+
+        public required byte Floor { get; set; }
+
+        public required double Latitude { get; set; }
+
+        public required double Longitude { get; set; }
+
+        public string? PostalCode { get; set; }
+
+        public string? DoorbellName { get; set; }
+
+        public string? Instructions { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
