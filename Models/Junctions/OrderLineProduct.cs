5bdd676fd54b4eb1944f3f8ee240d3ac27c176c9
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 22:08:43 +0200

    feat(Junctions): add OrderLineProduct junction class

---
+++ Models/Junctions/OrderLineProduct.cs
@@ -0,0 +1,21 @@
+﻿using Models.Entities;
+
+namespace Models.Junctions
+{
+    public class OrderLineProduct
+    {
+        public int ProductCount { get; set; }
+
+        public required string ProductFilterOptions { get; set; } // Stored as JSON combining all actual filters chosen for a Product in a ProductLine
+
+        public decimal LinePrice { get; set; } = 0m;
+
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; } = null!;
+
+        public Guid OrderLineId { get; set; }
+
+        public OrderLine OrderLine { get; set; } = null!;
+    }
+}
