1316dbf52a2e50e16f374feb7b00d2113963db91
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 21:52:08 +0200

    feat(Junction): add GlobalFilterProductOverride junction class

---
+++ Models/Junctions/GlobalFilterProductOverride.cs
@@ -0,0 +1,21 @@
+﻿using Models.Entities;
+
+namespace Models.Junctions
+{
+    public class GlobalFilterProductOverride
+    {
+        public required string NewFilterConfig { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid ProductId { get; set; }
+
+        public Product Product { get; set; } = null!;
+
+        public Guid GlobalFilterId { get; set; }
+
+        public GlobalFilter GlobalFilter { get; set; } = null!;
+    }
+}
