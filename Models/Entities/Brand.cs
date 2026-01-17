0f1be6a17b3317804ae6ac80654e518ad922681a
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 14:12:06 +0200

    feat(models): add Brand entity

---
+++ Models/Entities/Brand.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Entities
+{
+    public class Brand
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string LogoUrl { get; set; }
+
+        public string? Slug { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
