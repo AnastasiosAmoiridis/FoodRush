18a53f9bb12704eb0ac755fb94402e25f3423b70
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 20:16:03 +0200

    feat(models): add CodeDefenition entity

---
+++ Models/Entities/CodeDefinition.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Entities
+{
+    public class CodeDefinition
+    {
+        public Guid Id { get; set; }
+
+        public required string Description { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        public bool IsDeleted { get; set; } = false;
+
+        public Guid CodeId { get; set; }
+
+        public Code Code { get; set; } = null!;
+    }
+}
