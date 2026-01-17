ead9f9c74463860de5e29cda99f830a26b657cc7
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 14:22:18 +0200

    feat(models): add StoreAddress entity

---
+++ Models/Entities/StoreAddress.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Entities
+{
+    public class StoreAddress
+    {
+        public Guid Id { get; set; }
+
+        public required string City { get; set; }
+
+        public required string Street { get; set; }
+
+        public required double Latitude { get; set; }
+
+        public required double Longitude { get; set; }
+
+        public string? PostalCode { get; set; }
+    }
+}
