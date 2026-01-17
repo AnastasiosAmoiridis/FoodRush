842d7f02e5e4882a104b2735584bd9332a2f0961
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 22:02:50 +0200

    feat(Junctions): add OrderHeaderCode junction class

---
+++ Models/Junctions/OrderHeaderCode.cs
@@ -0,0 +1,15 @@
+﻿using Models.Entities;
+
+namespace Models.Junctions
+{
+    public class OrderHeaderCode
+    {
+        public Guid OrderHeaderId { get; set; }
+
+        public OrderHeader OrderHeader { get; set; } = null!;
+
+        public Guid CodeId { get; set; }
+
+        public Code Code { get; set; } = null!;
+    }
+}
