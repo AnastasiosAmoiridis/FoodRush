778b492fb612c7d459a93466e955978e55a9e56a
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 21:47:43 +0200

    feat(Junctions): add GlobalFilterGlobalCategory junction class

---
+++ Models/Junctions/GlobalFilterGlobalCategory.cs
@@ -0,0 +1,15 @@
+﻿using Models.Entities;
+
+namespace Models.Junctions
+{
+    public class GlobalFilterGlobalCategory
+    {
+        public Guid GlobalFilterId { get; set; }
+
+        public GlobalFilter GlobalFilter { get; set; } = null!;
+
+        public Guid GlobalProductCategoryId { get; set; }
+
+        public GlobalProductCategory GlobalProductCategory { get; set; } = null!;
+    }
+}
