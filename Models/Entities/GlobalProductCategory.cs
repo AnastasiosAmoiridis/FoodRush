4164b5e84bfc4e5833ccd069b223ceeff6cc4ef8
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 16:25:47 +0200

    feat(models): add GlobalProductCategory entity

---
+++ Models/Entities/GlobalProductCategory.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models.Entities
+{
+    public enum GlobalProductCategoryType
+    {
+        [Display(Name = "Πίτσες")]
+        Pizza,
+
+        [Display(Name = "Μπέργκερ")]
+        Burger,
+
+        [Display(Name = "Καφέδες")]
+        Coffee,
+
+        [Display(Name = "Μαγειρευτά")]
+        CookedDish,
+
+        [Display(Name = "Ζυμαρικά")]
+        Pasta,
+
+        [Display(Name = "Σουβλάκια")]
+        Souvlaki,
+
+        [Display(Name = "Σάντουιτς")]
+        Sandwich,
+
+        [Display(Name = "Γλυκά")]
+        Dessert,
+
+        [Display(Name = "Ποτά")]
+        Drink,
+
+        [Display(Name = "Σαλάτες")]
+        Salad,
+
+        [Display(Name = "Άλλο")]
+        Other
+    }
+
+    public class GlobalProductCategory
+    {
+        public Guid Id { get; set; }
+
+        public required GlobalProductCategoryType Type { get; set; }
+    }
+}
