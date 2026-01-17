d0190c9d7b85178fa0100f41de176815e667aaed
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-17 21:12:21 +0200

    feat(models): add Junctions folder and GlobalFilterBrandCategoryOverride junction table

---
+++ Models/Junctions/GlobalFilterBrandCategoryOverride.cs
@@ -0,0 +1,21 @@
+﻿using Models.Entities;
+
+namespace Models.Junctions
+{
+    public class GlobalFilterBrandCategoryOverride
+    {
+        public required string NewFilterConfig { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public Guid GlobalFilterId { get; set; }
+
+        public GlobalFilter GlobalFilter { get; set; } = null!;
+
+        public Guid BrandProductCategoryId { get; set; }
+
+        public BrandProductCategory BrandProductCategory { get; set; } = null!;
+    }
+}
