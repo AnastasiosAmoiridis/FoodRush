896c810ccab7aaaa28416cfd0421d0a6609937d4
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-19 16:16:37 +0200

    feat(Data): add FoodRushDbContext class

---
+++ Data/FoodRushDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data
+{
+    internal class FoodRushDbContext : DbContext
+    {
+        public FoodRushDbContext(DbContextOptions<FoodRushDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
