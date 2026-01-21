b7924ba5ff11c089939cadf3989e364eddb05f80
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-21 18:22:14 +0200

    fix: add Common project and EntityConstrints class to it. Reference Common on Data

---
+++ Common/EntityConstraints.cs
@@ -0,0 +1,33 @@
+﻿namespace Common
+{
+    public static class EntityConstraints
+    {
+        public const byte MAX_PHONE_LENGTH = 15;
+
+        public const byte MIN_PHONE_LENGTH = 10;
+
+        public const byte MAX_NAME_LENGTH = 200;
+
+        public const short MAX_URL_LENGTH = 500;
+
+        public const byte MAX_GLOBAL_PRODUCT_CATEGORY_TYPE_LENGTH = 100;
+
+        public const byte MAX_EMAIL_LENGTH = 200; public const byte MAX_POSTALCODE_LENGTH = 10;
+
+        public const short MAX_STREET_LENGTH = 300; public const short MAX_INSTRUCTION_LENGTH = 1000;
+
+        public const decimal MAX_RATING_VALUE = 5.0m; public const decimal MIN_RATING_VALUE = 0.0m;
+
+        public const string DEFAULT_SQL_KEY_VALUE = "NEWID()"; public const string CK_PRODUCT_BASEPRICE_MIN = "CK_Product_BasePrice_Min";
+
+        public const string CK_STORE_PHONE_MIN = "CK_Store_Phone_Min";
+
+        public const string CK_STORE_RATING_BOUNDARIES = "CK_Store_Rating_Boundaries";
+
+        public static readonly Dictionary<string, string> CheckContraints = new Dictionary<string, string> {
+            { CK_PRODUCT_BASEPRICE_MIN, "[BasePrice] >= 0.0" },
+            { CK_STORE_PHONE_MIN, "LEN([Phone]) >= 10" },
+            { CK_STORE_RATING_BOUNDARIES, "[Rating] >= 0.0 AND [Rating] <= 5.0" }
+        };
+    }
+}
