9c1582065351fe80a6986ea0d04c503708b61393
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-21 20:37:46 +0200

    feat(migrations): Add CreateGlobalProductCategory migration

---
+++ Data/Migrations/20260121183704_CreateGlobalProductCategory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateGlobalProductCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlobalProductCategories",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlobalProductCategories", x => x.Id);
+                    table.UniqueConstraint("AK_GlobalProductCategories_Type", x => x.Type);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlobalProductCategories");
+        }
+    }
+}
