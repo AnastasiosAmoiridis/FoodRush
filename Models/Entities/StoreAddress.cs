12dbe1eea04647af78ee026733bc2ef1c547355e
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-21 19:14:03 +0200

    fix(StoreAddress): convert Latitude and Longitude to decimal

--- Models/Entities/StoreAddress.cs
+++ Models/Entities/StoreAddress.cs
@@ -8,9 +8,9 @@
 
         public required string Street { get; set; }
 
-        public required double Latitude { get; set; }
+        public required decimal Latitude { get; set; }
 
-        public required double Longitude { get; set; }
+        public required decimal Longitude { get; set; }
 
         public string? PostalCode { get; set; }
 
