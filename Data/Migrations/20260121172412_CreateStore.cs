62b5ac065228f742202199951553dd46572fa7f2
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-21 19:27:12 +0200

    feat(migrations): add CreateStore migration

---
+++ Data/Migrations/20260121172412_CreateStore.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateStore : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, defaultValueSql: "NEWID()"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    AvgDeliveryTime = table.Column<short>(type: "smallint", nullable: false),
+                    Rating = table.Column<float>(type: "real", nullable: false),
+                    IsOpen = table.Column<bool>(type: "bit", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stores", x => x.Id);
+                    table.CheckConstraint("CK_Store_Phone_Min", "LEN([Phone]) >= 10");
+                    table.CheckConstraint("CK_Store_Rating_Boundaries", "[Rating] >= 0.0 AND [Rating] <= 5.0");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stores_Phone",
+                table: "Stores",
+                column: "Phone",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stores");
+        }
+    }
+}
