0ec3e74d660875547e666b3a4dcc8d14b7814dd9
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 14:49:51 +0200

    feat(migrations): add ConfigureOrderLineProductManyToMany migration

---
+++ Data/Migrations/20260122124854_ConfigureOrderLineProductManyToMany.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureOrderLineProductManyToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderLineProducts",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    OrderLineId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductCount = table.Column<int>(type: "int", nullable: false),
+                    ProductFilterOptions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinePrice = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLineProducts", x => new { x.OrderLineId, x.ProductId });
+                    table.CheckConstraint("CK_OrderLineProduct_ProductCount_Min", "[ProductCount] >= 1");
+                    table.ForeignKey(
+                        name: "FK_OrderLineProducts_OrderLines_OrderLineId",
+                        column: x => x.OrderLineId,
+                        principalTable: "OrderLines",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_OrderLineProducts_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLineProducts_ProductId",
+                table: "OrderLineProducts",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderLineProducts");
+        }
+    }
+}
