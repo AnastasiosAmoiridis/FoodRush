8c61d7b35340b4f452a7cc6ca97ee4ff634a6354
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 13:41:44 +0200

    feat(migrations): add ConfigureCustomerOrderHeaderOneToMany migration

---
+++ Data/Migrations/20260122114053_ConfigureCustomerOrderHeaderOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureCustomerOrderHeaderOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "OrderHeaders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeaders_CustomerId",
+                table: "OrderHeaders",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_Customers_CustomerId",
+                table: "OrderHeaders",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_Customers_CustomerId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderHeaders_CustomerId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "OrderHeaders");
+        }
+    }
+}
