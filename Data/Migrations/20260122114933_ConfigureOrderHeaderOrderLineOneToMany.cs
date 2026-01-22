dfeb62229b36c9f183b84e1a0d9ae68baac93682
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 13:49:53 +0200

    feat(migrations): add ConfigureOrderHeaderOrderLineOneToMany migration

---
+++ Data/Migrations/20260122114933_ConfigureOrderHeaderOrderLineOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureOrderHeaderOrderLineOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderHeaderId",
+                table: "OrderLines",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLines_OrderHeaderId",
+                table: "OrderLines",
+                column: "OrderHeaderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderLines_OrderHeaders_OrderHeaderId",
+                table: "OrderLines",
+                column: "OrderHeaderId",
+                principalTable: "OrderHeaders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderLines_OrderHeaders_OrderHeaderId",
+                table: "OrderLines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderLines_OrderHeaderId",
+                table: "OrderLines");
+
+            migrationBuilder.DropColumn(
+                name: "OrderHeaderId",
+                table: "OrderLines");
+        }
+    }
+}
