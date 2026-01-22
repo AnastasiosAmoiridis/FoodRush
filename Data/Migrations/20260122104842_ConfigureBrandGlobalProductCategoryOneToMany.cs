a33f1af5ae33461cf52842c86d2bc9a2412acc67
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 12:50:11 +0200

    feat(migrations):  add ConfigureBrandGlobalProductCategoryOneToMany migration

---
+++ Data/Migrations/20260122104842_ConfigureBrandGlobalProductCategoryOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureBrandGlobalProductCategoryOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PrimaryProductCategoryId",
+                table: "Brands",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Brands_PrimaryProductCategoryId",
+                table: "Brands",
+                column: "PrimaryProductCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Brands_GlobalProductCategories_PrimaryProductCategoryId",
+                table: "Brands",
+                column: "PrimaryProductCategoryId",
+                principalTable: "GlobalProductCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Brands_GlobalProductCategories_PrimaryProductCategoryId",
+                table: "Brands");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Brands_PrimaryProductCategoryId",
+                table: "Brands");
+
+            migrationBuilder.DropColumn(
+                name: "PrimaryProductCategoryId",
+                table: "Brands");
+        }
+    }
+}
