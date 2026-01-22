3648009f801c62c22cac339a69f67c117d05e8ca
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 16:53:36 +0200

    feat(migrations): add ConfigureOrderHeaderPaymentCodeOneToMany migration

--- Data/Migrations/20260122145241_ConfigureOrderHeaderPaymentCodeOneToMany.Designer.cs
+++ Data/Migrations/20260122145241_ConfigureOrderHeaderPaymentCodeOneToMany.Designer.cs
@@ -4,6 +4,7 @@ using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Data.Migrations
 {
     [DbContext(typeof(FoodRushDbContext))]
-    partial class FoodRushDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260122145241_ConfigureOrderHeaderPaymentCodeOneToMany")]
+    partial class ConfigureOrderHeaderPaymentCodeOneToMany
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20260122145241_ConfigureOrderHeaderPaymentCodeOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureOrderHeaderPaymentCodeOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "PaymentCodeId",
+                table: "OrderHeaders",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderHeaders_PaymentCodeId",
+                table: "OrderHeaders",
+                column: "PaymentCodeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderHeaders_Codes_PaymentCodeId",
+                table: "OrderHeaders",
+                column: "PaymentCodeId",
+                principalTable: "Codes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderHeaders_Codes_PaymentCodeId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderHeaders_PaymentCodeId",
+                table: "OrderHeaders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentCodeId",
+                table: "OrderHeaders");
+        }
+    }
+}
