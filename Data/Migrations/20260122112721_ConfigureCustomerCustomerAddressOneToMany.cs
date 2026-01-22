95c542b0ac1296051314b20ad127eeae53f5d216
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 13:27:51 +0200

    feat(migrations): add ConfigureCustomerCustomerAddressOneToMany migration

---
+++ Data/Migrations/20260122112721_ConfigureCustomerCustomerAddressOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureCustomerCustomerAddressOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CustomerId",
+                table: "CustomerAddresses",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerAddresses_CustomerId",
+                table: "CustomerAddresses",
+                column: "CustomerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerAddresses_Customers_CustomerId",
+                table: "CustomerAddresses",
+                column: "CustomerId",
+                principalTable: "Customers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerAddresses_Customers_CustomerId",
+                table: "CustomerAddresses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerAddresses_CustomerId",
+                table: "CustomerAddresses");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerId",
+                table: "CustomerAddresses");
+        }
+    }
+}
