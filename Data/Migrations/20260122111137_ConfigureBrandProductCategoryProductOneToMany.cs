94c19746bd04c3c81678d10c558e1dfb401b25cd
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 13:17:11 +0200

    feat(migrations): add ConfigureBrandProductCategoryProductOneToMany migration

---
+++ Data/Migrations/20260122111137_ConfigureBrandProductCategoryProductOneToMany.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureBrandProductCategoryProductOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "BrandProductCategoryId",
+                table: "Products",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_BrandProductCategoryId",
+                table: "Products",
+                column: "BrandProductCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_BrandProductCategories_BrandProductCategoryId",
+                table: "Products",
+                column: "BrandProductCategoryId",
+                principalTable: "BrandProductCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_BrandProductCategories_BrandProductCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_BrandProductCategoryId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "BrandProductCategoryId",
+                table: "Products");
+        }
+    }
+}
