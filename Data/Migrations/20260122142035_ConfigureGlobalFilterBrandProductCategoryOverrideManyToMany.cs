057cca5c011be04d55ba63a159dcb868c6379c2d
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-22 16:21:20 +0200

    feat(migrations): add ConfigureGlobalFilterBrandProductCategoryOverrideManyToMany migration

---
+++ Data/Migrations/20260122142035_ConfigureGlobalFilterBrandProductCategoryOverrideManyToMany.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureGlobalFilterBrandProductCategoryOverrideManyToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GlobalFilterBrandCategoryOverrides",
+                columns: table => new
+                {
+                    GlobalFilterId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BrandProductCategoryId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NewFilterConfig = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GlobalFilterBrandCategoryOverrides", x => new { x.BrandProductCategoryId, x.GlobalFilterId });
+                    table.ForeignKey(
+                        name: "FK_GlobalFilterBrandCategoryOverrides_BrandProductCategories_BrandProductCategoryId",
+                        column: x => x.BrandProductCategoryId,
+                        principalTable: "BrandProductCategories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GlobalFilterBrandCategoryOverrides_GlobalFilters_GlobalFilterId",
+                        column: x => x.GlobalFilterId,
+                        principalTable: "GlobalFilters",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GlobalFilterBrandCategoryOverrides_GlobalFilterId",
+                table: "GlobalFilterBrandCategoryOverrides",
+                column: "GlobalFilterId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GlobalFilterBrandCategoryOverrides");
+        }
+    }
+}
