987d2d00076f5b67825d90adce05194e3c6d950d
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-23 18:29:18 +0200

    feat(Services): add AddServices IServiceCollection extension to register all Service services to DI container

---
+++ Services/DepedencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Services
+{
+    public static class DepedencyInjection
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {           
+            return services;
+        }
+    }
+}
