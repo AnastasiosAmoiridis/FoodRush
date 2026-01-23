4436b4c161b1756d791ee60404a19efe9bf3dcec
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-23 15:44:39 +0200

    fix(Code):  remove Name and make Description not null

--- Models/Entities/Code.cs
+++ Models/Entities/Code.cs
@@ -6,9 +6,7 @@
     {
         public Guid Id { get; set; }
 
-        public required string Name { get; set; }
-
-        public string? Description { get; set; }
+        public required string Description { get; set; }
 
         public bool IsActive { get; set; } = true;
 
