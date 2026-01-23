79da8ef55ed8aa559d03395f8e39bcdea46bf400
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-23 15:07:53 +0200

    feat(migrations): add MakeBrandNameUnique migration

---
+++ Data/Migrations/20260123130705_MakeBrandNameUnique.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeBrandNameUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddUniqueConstraint(
+                name: "AK_Brands_Name",
+                table: "Brands",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "AK_Brands_Name",
+                table: "Brands");
+        }
+    }
+}
