339ba66a24c1daddc8f9af2316902d1a8eaaffc3
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-23 15:14:26 +0200

    feat(migrations): add MakeBrandProductCategoryNameUniquePerBrand migration

---
+++ Data/Migrations/20260123131223_MakeBrandProductCategoryNameUniquePerBrand.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeBrandProductCategoryNameUniquePerBrand : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BrandProductCategories_Name_BrandId",
+                table: "BrandProductCategories",
+                columns: new[] { "Name", "BrandId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BrandProductCategories_Name_BrandId",
+                table: "BrandProductCategories");
+        }
+    }
+}
