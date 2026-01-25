afa15582cf8b2052c741b946630a3be5ca20eaf3
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-25 23:00:16 +0200

    feat(DTOs): add general BrandDto

---
+++ Services/DTOs/BrandDto.cs
@@ -0,0 +1,27 @@
+﻿using Models.Entities;
+
+namespace Services.DTOs
+{
+    public class BrandDto
+    {
+        public Guid Id { get; set; }
+
+        public required string Name { get; set; }
+
+        public required string LogoUrl { get; set; }
+
+        public string? Slug { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public Guid PrimaryProductCategoryId { get; set; }
+
+        public GlobalProductCategory PrimaryProductCategory { get; set; }
+
+        public ICollection<Store> Stores { get; set; } = new List<Store>();
+
+        public ICollection<BrandProductCategory> BrandProductCategories { get; set; }
+    }
+}
