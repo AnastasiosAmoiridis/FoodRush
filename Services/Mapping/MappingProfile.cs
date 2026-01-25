279508a156c77f632ca42284ae90f3e1017d5dff
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-25 23:00:50 +0200

    feat(Mapping): add mapping rules for Brand -> BrandDto

---
+++ Services/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Models.Entities;
+using Services.DTOs;
+
+namespace Services.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Brand, BrandDto>();
+        }
+    }
+}
