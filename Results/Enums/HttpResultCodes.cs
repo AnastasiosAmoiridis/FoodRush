29437bea0ffbda8cb615a4d97931aa9e591e3259
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-25 22:52:24 +0200

    feat(Results): add Enums folder and HttpResultCodes enum

---
+++ Results/Enums/HttpResultCodes.cs
@@ -0,0 +1,14 @@
+﻿namespace Results.Enums
+{
+    public enum HttpResultCode
+    {
+        Ok = 200,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        NotFound = 404,
+        InternalServerError = 500,
+    }
+
+}
+
