fac59515d601342df925317dd7e187c6102b1ec9
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-25 22:52:56 +0200

    feat(Results): add HttpResultBase class

---
+++ Results/HttpResultBase.cs
@@ -0,0 +1,28 @@
+﻿
+using Results.Enums;
+
+namespace Results
+{
+    public class HttpResultBase : ResultBase
+    {
+        public HttpResultCode Code { get; private set; }
+
+        public void SetSucess()
+        {
+            base.SetSuccess();
+            Code = HttpResultCode.Ok;
+        }
+
+        public void FailWithCode(HttpResultCode code, string errorDetails)
+        {
+            base.Fail(errorDetails);
+            Code = code;
+        }
+
+        public override void FailWithValidationErrors(ICollection<ValidationError> validationErrors)
+        {
+            base.FailWithValidationErrors(validationErrors);
+            Code = HttpResultCode.BadRequest;
+        }
+    }
+}
