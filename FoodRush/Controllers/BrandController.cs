6fd494f22d9c94905ed4f834a11415d36e05651e
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-25 23:05:37 +0200

    feat(Controllers): add BrandController along with a method to get Brand by name

---
+++ FoodRush/Controllers/BrandController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.DTOs;
+using Services.Interfaces;
+using Results;
+
+namespace FoodRush.Controllers
+{
+    [ApiController()]
+    [Route("api/[controller]")]
+    public class BrandController : ControllerBase
+    {
+        IBrandService _service;
+
+        public BrandController(IBrandService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{name}")]
+        [Produces("application/json")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Result<BrandDto>>> GetByNameAsync([FromRoute] string name)
+        {
+
+            Result<BrandDto>? response = await _service.GetByNameAsync(name);
+            if (response.Success)
+            {
+                return Ok(response);
+            }
+            return StatusCode((int)response.Code, response.ErrorDetails);
+        }
+    }
+}
