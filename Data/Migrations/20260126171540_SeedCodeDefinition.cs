b671472cc6dfa99e705a51cb8d4353527efcb1e4
Anastasio Amiridis <anamoiridis@gmail.com>
2026-01-27 18:18:22 +0200

    feat(Migrations): add SeedCodeDefinition migration

---
+++ Data/Migrations/20260126171540_SeedCodeDefinition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCodeDefinition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "CodeDefinitions",
+                columns: new[] { "Id", "Description", "IsActive", "IsDeleted" },
+                values: new object[,]
+                {
+                    { new Guid("3c9e1f72-0a5d-4e8b-b7a4-9d6f2c1e4b22"), "OrderStatus", true, false },
+                    { new Guid("8f2a6c3e-4b91-4d6c-9c3f-1a7e9f2b0a11"), "Payment", true, false }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "CodeDefinitions",
+                keyColumn: "Id",
+                keyValue: new Guid("3c9e1f72-0a5d-4e8b-b7a4-9d6f2c1e4b22"));
+
+            migrationBuilder.DeleteData(
+                table: "CodeDefinitions",
+                keyColumn: "Id",
+                keyValue: new Guid("8f2a6c3e-4b91-4d6c-9c3f-1a7e9f2b0a11"));
+        }
+    }
+}
