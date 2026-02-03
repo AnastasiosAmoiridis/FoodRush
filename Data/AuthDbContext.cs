ccb0907a92492e07b7f80b9000b1e784606e4afb
Anastasio Amiridis <anamoiridis@gmail.com>
2026-02-03 13:53:34 +0200

    feat(Data): set RefreshToken to AuthDbContext and and RefreshTokenConfig

--- Data/AuthDbContext.cs
+++ Data/AuthDbContext.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Models.Entities.Auth;
 
 namespace Data
 {
     public class AuthDbContext : IdentityDbContext<IdentityUser>
     {
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AuthDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Data/EntityConfigurations/Auth/RefreshTokenConfig.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Models.Entities.Auth;
+
+namespace Data.EntityConfigurations.Auth
+{
+    internal sealed class RefreshTokenConfig : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("RefreshTokens");
+
+            builder.HasKey(rt => rt.Id);
+
+            builder.Property(rt => rt.Id)
+                   .HasDefaultValueSql(EntityConstraints.DEFAULT_SQL_KEY_VALUE);
+        }
+    }
+}
