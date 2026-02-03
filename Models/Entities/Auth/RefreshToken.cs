2f506a3de17fcd48abfc6a67b8263670eb5a25ad
Anastasio Amiridis <anamoiridis@gmail.com>
2026-02-03 13:44:00 +0200

    feat(Entities): add RefreshToken entity

---
+++ Models/Entities/Auth/RefreshToken.cs
@@ -0,0 +1,17 @@
+﻿namespace Models.Entities.Auth
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }
+
+        public required string Token { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime? Revoked { get; set; }
+
+        public string? ReasonRevoked { get; set; }   
+    }
+}
