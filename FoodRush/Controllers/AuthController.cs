c14a2e50daa9f6f0d1d7c486c9ebb07a55a2e786
Anastasio Amiridis <anamoiridis@gmail.com>
2026-02-04 14:19:50 +0200

    feat(Controllers): add AuthController

---
+++ FoodRush/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Interfaces;
+
+namespace FoodRush.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _service;
+
+        public AuthController(IAuthService service)
+        {
+            _service = service;
+        }
+    }
+}
