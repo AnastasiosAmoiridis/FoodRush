d68e412b56b6d9e0d642e083dddd12a87b89cfef
Anastasio Amiridis <anamoiridis@gmail.com>
2026-02-04 13:06:41 +0200

    feat(AuthMigrations): add ConfigureIdentityUserHasRefreshTokenOneToMany migration

---
+++ Data/AuthMigrations/20260204110549_ConfigureIdentityUserHasRefreshTokenOneToMany.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.AuthMigrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureIdentityUserHasRefreshTokenOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IdentityUserId",
+                table: "RefreshTokens",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RefreshTokens_IdentityUserId",
+                table: "RefreshTokens",
+                column: "IdentityUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_IdentityUserId",
+                table: "RefreshTokens",
+                column: "IdentityUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_RefreshTokens_AspNetUsers_IdentityUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_RefreshTokens_IdentityUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "IdentityUserId",
+                table: "RefreshTokens");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
