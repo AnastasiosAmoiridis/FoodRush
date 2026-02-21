4017132cff19e0096aaf9123125ef9e68c9da577
Anastasio Amiridis <anamoiridis@gmail.com>
2026-02-21 13:23:07 +0200

    feat: add a controller base class with a method to resolve Http responses

---
+++ FoodRush/Controllers/FoodRushBaseControllerBase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Results;
+using Results.Enums;
+
+namespace FoodRush.Controllers
+{
+
+    [ApiController]
+    public abstract class FoodRushBaseControllerBase : ControllerBase
+    {
+        protected ActionResult<T> HandleResut<T>(T result) where T : ResultBase
+        {
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+
+            return result.FailureType switch
+            {
+                ResultFailureType.Validation => BadRequest(result),
+                ResultFailureType.BusinessRuleViolation => BadRequest(result),
+                ResultFailureType.NotFound => NotFound(result),
+                ResultFailureType.Authentication => Unauthorized(result),
+                ResultFailureType.Authorization => Forbid(),
+                ResultFailureType.TransactionFailure => StatusCode(StatusCodes.Status500InternalServerError, result),
+                _ => StatusCode(StatusCodes.Status500InternalServerError, result)
+            };
+        }
+    }
+}
