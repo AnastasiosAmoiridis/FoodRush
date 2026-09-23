9c190bf375a279a35a92b1dc995e3fdb2a0d2e56
Anastasios Amoiridis <anamoiridis@gmail.com>
2026-09-23 12:24:41 +0300

    feat(FoodRush): centralize application options registration

---
+++ FoodRush/Options/OptionsRegistration.cs
@@ -0,0 +1,16 @@
+﻿using Models.Options.Auth;
+
+namespace FoodRush.Options
+{
+    public static class OptionsRegistration
+    {
+        public static IServiceCollection AddApplicationOptions(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddOptions<JWTOptions>().Bind(configuration.GetSection(AuthOptions.Auth).GetSection(JWTOptions.JWT));
+
+            services.AddOptions<AuthOptions>().Bind(configuration.GetSection(AuthOptions.Auth));
+
+            return services;
+        }
+    }
+}
